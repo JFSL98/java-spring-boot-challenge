b51f7b041bfe6a3739fd9b1b4b136547f3d6c9d4
João Lopes <lopes.jfsl98@gmail.com>
2025-02-26 18:29:50 +0000

    Initial commit

---
+++ src/test/java/com/example/javaspringchallenge/JavaSpringChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
