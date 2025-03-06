282756a438aef8c9d8363defcf47f4dd5b3da0f3
João Lopes <lopes.jfsl98@gmail.com>
2025-03-06 00:48:31 +0000

    Controllers return ResponseEntity to control the HTTP status code

--- rest/src/main/java/com/example/rest/controllers/CalculatorController.java
+++ rest/src/main/java/com/example/rest/controllers/CalculatorController.java
@@ -4,6 +4,8 @@ import com.example.rest.dtos.CalculationRequestDTO;
 import com.example.rest.dtos.CalculationResponseDTO;
 import com.example.rest.middleware.kafka.CalculatorProducer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,46 +25,49 @@ public class CalculatorController {
     }
 
     @GetMapping("/sum")
-    public CalculationResponseDTO sum(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
+    public ResponseEntity<CalculationResponseDTO> sum(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
         CalculationRequestDTO calculationRequestDTO = new CalculationRequestDTO("sum", a, b);
-        try {
-            return calculatorProducer.handleCalculationRequest(calculationRequestDTO);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new CalculationResponseDTO("Unexpected error: " + e.getMessage());
-        }
+        return getCalculationResponseDTOResponseEntity(calculationRequestDTO);
     }
 
     @GetMapping("/subtract")
-    public CalculationResponseDTO subtract(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
+    public ResponseEntity<CalculationResponseDTO> subtract(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
         CalculationRequestDTO calculationRequestDTO = new CalculationRequestDTO("subtract", a, b);
-        try {
-            return calculatorProducer.handleCalculationRequest(calculationRequestDTO);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new CalculationResponseDTO("Unexpected error: " + e.getMessage());
-        }
+        return getCalculationResponseDTOResponseEntity(calculationRequestDTO);
     }
 
     @GetMapping("/multiply")
-    public CalculationResponseDTO multiply(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
+    public ResponseEntity<CalculationResponseDTO> multiply(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
         CalculationRequestDTO calculationRequestDTO = new CalculationRequestDTO("multiply", a, b);
-        try {
-            return calculatorProducer.handleCalculationRequest(calculationRequestDTO);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new CalculationResponseDTO("Unexpected error: " + e.getMessage());
-        }
+        return getCalculationResponseDTOResponseEntity(calculationRequestDTO);
     }
 
     @GetMapping("/divide")
-    public CalculationResponseDTO divide(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
+    public ResponseEntity<CalculationResponseDTO> divide(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
         CalculationRequestDTO calculationRequestDTO = new CalculationRequestDTO("divide", a, b);
+        return getCalculationResponseDTOResponseEntity(calculationRequestDTO);
+    }
+
+    private ResponseEntity<CalculationResponseDTO> getCalculationResponseDTOResponseEntity(CalculationRequestDTO calculationRequestDTO) {
         try {
-            return calculatorProducer.handleCalculationRequest(calculationRequestDTO);
+            CalculationResponseDTO response = calculatorProducer.handleCalculationRequest(calculationRequestDTO);
+
+            HttpStatus status;
+            if (response.getResult() != null) {
+                status = HttpStatus.OK;
+            } else if (response.getErrorMessage().compareTo("Divide by zero") == 0) {
+                status = HttpStatus.BAD_REQUEST;
+            } else {
+                status = HttpStatus.INTERNAL_SERVER_ERROR;
+            }
+
+            return new ResponseEntity<>(response, status);
         } catch (Exception e) {
             e.printStackTrace();
-            return new CalculationResponseDTO("Unexpected error: " + e.getMessage());
+            return new ResponseEntity<>(
+                    new CalculationResponseDTO("Unexpected error: " + e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
         }
     }
 }
